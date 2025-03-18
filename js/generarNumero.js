621f4d13d2047fc0d22b2a71a1949f3ca71662a0
farit <faritteranacosta@gmail.com>
2025-03-18 12:25:41 -0500

    Add game logic and UI for "Numero Magico" guessing game and new version

--- js/generarNumero.js
+++ js/generarNumero.js
@@ -1,2 +1,37 @@
-    
+let numeroAleatorio;
+function generarNumero() {
+    if (document.getElementById("intentos").innerHTML == 0) {
+        numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+        console.log(numeroAleatorio);
+    } else {
+        console.log("Ya se genero un numero");
+    }
+}
+function comparar() {
+        let entrada = document.getElementById("numero").value;
+        document.getElementById("intentos").innerHTML++;
+        document.getElementById("ultimo-intentos").innerHTML = document.getElementById("numero").value;
+
+        if (entrada == numeroAleatorio) {
+            document.getElementById("resultado").innerHTML = "Correcto";
+            alert("Felicidades has adivinado el numero");
+            location.reload();
+            return;
+        } else if (entrada > numeroAleatorio) {
+            document.getElementById("resultado").innerHTML = "Te doy una pista el numero es menor que " + entrada;
+        } else {
+            document.getElementById("resultado").innerHTML = "Te ayudo un poco el numero es mayor que " + entrada;
+        }
+        gameover();
+}
+
+function gameover() {
+    if (document.getElementById("intentos").innerHTML == 10) {
+        alert("GAME OVER");
+        alert("El numero es: " + numeroAleatorio);
+        location.reload();
+    } else if (document.getElementById("intentos").innerHTML == 5) {
+        alert("Te quedan 5 intentos escoge bien tu siguiente numero");
+    }
+}
 
