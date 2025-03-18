cf9cbfacccb3783886eb97b582c4c4138358ef0a
farit <faritteranacosta@gmail.com>
2025-03-18 14:28:21 -0500

    Enhance guessing game: update styles and improve input validation logic

--- js/generarNumero.js
+++ js/generarNumero.js
@@ -9,26 +9,30 @@ function generarNumero() {
 }
 function comparar() {
         let entrada = document.getElementById("numero").value;
-        document.getElementById("intentos").innerHTML++;
-        document.getElementById("ultimo-intentos").innerHTML = document.getElementById("numero").value;
+        console.log(entrada);
+        if(entrada == ""){
+            alert("Se debe ingresar un numero");
+        }else{
+            document.getElementById("intentos").innerHTML++;
+            document.getElementById("ultimo-intentos").innerHTML = document.getElementById("numero").value;
 
-        if (entrada == numeroAleatorio) {
-            document.getElementById("resultado").innerHTML = "Correcto";
-            alert("Felicidades has adivinado el numero");
-            location.reload();
-            return;
-        } else if (entrada > numeroAleatorio) {
-            document.getElementById("resultado").innerHTML = "Te doy una pista el numero es menor que " + entrada;
-        } else {
-            document.getElementById("resultado").innerHTML = "Te ayudo un poco el numero es mayor que " + entrada;
+            if (entrada == numeroAleatorio) {
+                document.getElementById("resultado").innerHTML = "Correcto";
+                alert("Felicidades has adivinado el numero");
+                location.reload();
+                return;
+            } else if (entrada > numeroAleatorio) {
+                document.getElementById("resultado").innerHTML = "Te doy una pista el numero es menor que " + entrada;
+            } else {
+                document.getElementById("resultado").innerHTML = "Te ayudo un poco el numero es mayor que " + entrada;
+            }
+            gameover();
         }
-        gameover();
 }
 
 function gameover() {
     if (document.getElementById("intentos").innerHTML == 10) {
-        alert("GAME OVER");
-        alert("El numero es: " + numeroAleatorio);
+        alert("GAME OVER"+"\nEl numero era: "+numeroAleatorio);
         location.reload();
     } else if (document.getElementById("intentos").innerHTML == 5) {
         alert("Te quedan 5 intentos escoge bien tu siguiente numero");
