704d63face14ca234a20f5078b6fbef326537a90
farit <faritteranacosta@gmail.com>
2025-03-18 22:19:47 -0500

    Add audio feedback and enhance UI for guessing game

--- js/generarNumero.js
+++ js/generarNumero.js
@@ -1,4 +1,5 @@
 let numeroAleatorio;
+let listaIntentos = [];
 function generarNumero() {
     if (document.getElementById("intentos").innerHTML == 0) {
         numeroAleatorio = Math.floor(Math.random() * 100) + 1;
@@ -11,12 +12,15 @@ function comparar() {
         let entrada = document.getElementById("numero").value;
         console.log(entrada);
         if(entrada == ""){
+            playAudio("error");
             alert("Se debe ingresar un numero");
         }else{
             document.getElementById("intentos").innerHTML++;
-            document.getElementById("ultimo-intentos").innerHTML = document.getElementById("numero").value;
-
+            document.getElementById("ultimo-intentos").innerHTML = entrada;
+            listaIntentos.push(entrada);
+            
             if (entrada == numeroAleatorio) {
+                playAudio("win");
                 document.getElementById("resultado").innerHTML = "Correcto";
                 alert("Felicidades has adivinado el numero");
                 location.reload();
@@ -27,14 +31,22 @@ function comparar() {
                 document.getElementById("resultado").innerHTML = "Te ayudo un poco el numero es mayor que " + entrada;
             }
             gameover();
+            console.log(listaIntentos);
         }
+}
+
+function playAudio(au) {
+    let audio = document.getElementById(au);
+    audio.play();
 }
 
 function gameover() {
     if (document.getElementById("intentos").innerHTML == 10) {
+        document.getElementById("numero").disabled = true;
+        playAudio("gameOver");
         alert("GAME OVER"+"\nEl numero era: "+numeroAleatorio);
-        location.reload();
     } else if (document.getElementById("intentos").innerHTML == 5) {
+        playAudio("al");
         alert("Te quedan 5 intentos escoge bien tu siguiente numero");
     }
 }
